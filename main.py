455804436c3f4be1b03e104e4d539b3df6785321
RyanMcL <c21431604@mytudublin.ie>
2022-09-27 13:54:22 +0100

    1

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Hello World")
+b = int(input("Please insert a number "))
+c = int(input("Please insert another number "))
+a = b+c
+print(a)
+if a > 2:
+    print("The number is a number")
+
+else:
+    print("The number is not a number")
+
+student_name = input('What is your name? >')
+print("Hello there, ", student_name)
